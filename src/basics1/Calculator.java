0c752de179fcc4e2fe8512819d7eda997867f74c
Javaclass <Javaclass@15000538cpu.fec.org>
2017-09-26 15:32:46 -0500

    Calculation Sum

--- src/basics1/Calculator.java
+++ src/basics1/Calculator.java
@@ -3,8 +3,16 @@ package basics1;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//System.out.println(calculateSum(6, 13)    
 	}
+	
+static int calculateSum(int a, int b){ 
+     return a+b;
+    }
+
+static void Printsum(int a, int b) {
+System.out.println(a+b);
+
+}
 
 }
