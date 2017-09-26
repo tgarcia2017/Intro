3851c783c207b8b68a7bb3bd7cc2b4194edb7c63
Javaclass <Javaclass@15000538cpu.fec.org>
2017-09-26 10:18:28 -0500

    My first class using Eclipse. Thank you Ramone for your help.

---
+++ src/basics1/GarciaFirstClass.java
@@ -0,0 +1,10 @@
+package basics1;
+
+public class GarciaFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Learning  Java Software 2017");
+	}
+
+}
