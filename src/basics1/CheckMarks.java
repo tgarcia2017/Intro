e5dbeafa9b461ed84941ffdd1cee97627fdffa6a
Javaclass <Javaclass@15000538cpu.fec.org>
2017-09-27 13:16:55 -0500

    Grading Student

---
+++ src/basics1/CheckMarks.java
@@ -0,0 +1,35 @@
+package basics1;
+
+public class CheckMarks {
+
+	public static void main(String[] args) {
+		gradeStudent(73);
+	}
+
+	// TODO Auto-generated method stub
+
+	static void gradeStudent(int studentScore) {
+
+		// if studentScore is 50-60 print D
+		if (studentScore >= 50 && studentScore < 60) {
+			System.out.println("Grade D");
+		}
+
+		// if studentScore is 61-70 print C
+		else if (studentScore >= 60 && studentScore < 70) {
+			System.out.println("Grade C");
+
+		}
+
+		// if studentScore is 71-80 print B
+		else if (studentScore >= 71 && studentScore < 80) {
+			System.out.println("Grade B");
+		}
+
+		// if studentScore is 81-100 print A
+		else if (studentScore >= 71 && studentScore < 80) {
+			System.out.println("Grade B");
+
+		}
+	}
+}
