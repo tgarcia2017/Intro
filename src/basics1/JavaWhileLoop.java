62030eddee815833a5f52744d68419d23a67a2c4
Javaclass <Javaclass@15000538cpu.fec.org>
2017-09-27 15:05:00 -0500

    Java While Loop

---
+++ src/basics1/JavaWhileLoop.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class JavaWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int loopname =1; 
+		
+		while(loopname<=12) {
+			System.out.println("Tracy");
+			loopname++;
+		}
+	}
+
+}
