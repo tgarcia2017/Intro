e2d8435895ce6076f6d1d152409a2e3e5d88ecd6
JavaClass <JavaClass@15000538cpu.fec.org>
2017-10-03 11:30:21 -0500

    The Parrot who talks loud before and after hour.

---
+++ src/basics1/LoudTalkingParrot.java
@@ -0,0 +1,22 @@
+package basics1;
+
+public class LoudTalkingParrot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println(parrot(true, 7));
+	}
+		static boolean parrot(boolean talking, int hour) {	 
+			 
+			 if(talking &&( hour<7 || hour> 20)) {
+				 return true; 
+		}
+			 else {
+			 }
+				 return false;
+             }
+						 
+		}
+			 
+			 
+		
