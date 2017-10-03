5e06c2bbb80cb919e299e1a759f7174a61fd87ec
JavaClass <JavaClass@15000538cpu.fec.org>
2017-10-03 13:32:52 -0500

    Even or Odd

---
+++ src/basics1/EvenOrOdd.java
@@ -0,0 +1,20 @@
+package basics1;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		evenOrOdd(7);
+	}
+		static void evenOrOdd(int i) {
+			
+			if (i%2 == 0) {
+				System.out.println("Number is even");
+			}
+			
+			else {
+				System.out.println("Number is odd");
+			}
+		}
+		
+		
+	}
