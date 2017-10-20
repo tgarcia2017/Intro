8ecb04772663695f52e8a102994d216e361106dc
Javaclass <Javaclass@15000538cpu.fec.org>
2017-10-20 13:19:40 -0500

    ArrayOfPlaces

---
+++ src/Arrayplaceslist.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Arrayplaceslist {
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+ ArrayList<String> places  = new ArrayList<String>();
+
+ places.add("Europe");
+ places.add("Italy");
+ places.add("Bosnia");
+ places.add("Germany");
+ places.add("Austria");
+		
+	System.out.println(places);
+	
+	
+	System.out.println(places);
+}
+
+}
+	
